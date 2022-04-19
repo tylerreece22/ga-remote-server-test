5233f7e896ea47e26ba85983b2a897340a78c0e5
Tyler Reece <tylerreece@Tyler-Reece.local>
2022-04-19 16:53:29 -0400

    Added ga and firebase hosting

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,26 @@
 <script>
+	// Import the functions you need from the SDKs you need
+	import { initializeApp } from "firebase/app";
+	import { getAnalytics } from "firebase/analytics";
+	// TODO: Add SDKs for Firebase products that you want to use
+	// https://firebase.google.com/docs/web/setup#available-libraries
+
+	// Your web app's Firebase configuration
+	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+	const firebaseConfig = {
+		apiKey: "AIzaSyCIneXr4eeq1mWP7UTqH3cyJdjCf3Pfcqc",
+		authDomain: "ga-server-test.firebaseapp.com",
+		projectId: "ga-server-test",
+		storageBucket: "ga-server-test.appspot.com",
+		messagingSenderId: "443284951214",
+		appId: "1:443284951214:web:d017976e10ccd757c91d3f",
+		measurementId: "G-LZYBQQWKBZ"
+	};
+
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+	const analytics = getAnalytics(app);
+
 	export let name;
 </script>
 
